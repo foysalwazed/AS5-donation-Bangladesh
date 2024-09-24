0ead4b3edb80fc31af5de786a7d9a691541af8f0
unknown <you@example.com>
2024-09-24 15:10:19 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById('btn-donation-money').addEventListener('click',function(event){
+    event.preventDefault();
+
+    // const donationMoney = document.getElementById('input-add-money').value;
+    // const donationMoney = parseFloat(addMoney);
+
+
+});
---
+++ utilites.js
@@ -0,0 +1,1 @@
+consol.log('utilities file add')
