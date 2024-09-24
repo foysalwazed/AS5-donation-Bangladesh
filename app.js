af9b1e36ea45ee962880b244f63265e082f75c71
unknown <you@example.com>
2024-09-24 16:31:07 +0600

    first blog commit

--- app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('btn-donation-money').addEventListener('click',function(event){
-    event.preventDefault();
-
-    // const donationMoney = document.getElementById('input-add-money').value;
-    // const donationMoney = parseFloat(addMoney);
-
-
-});
